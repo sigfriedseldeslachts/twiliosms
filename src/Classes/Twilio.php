5ca6f2336707e385ae244673e84c36ba98e786a9
Sigfried Seldeslachts <sigfriedseldeslachts@gmail.com>
2017-03-07 21:37:50 +0100

    Now using PHP chaining
    Still in progress

--- src/Classes/Twilio.php
+++ src/Classes/Twilio.php
@@ -2,75 +2,167 @@
 
 namespace Sigfriedseldeslachts\twiliosms\Classes;
 
+use Exception;
 use Twilio\Rest\Client;
 
 class Twilio
 {
     protected $client;
+    public $method;
+    public $recipient;
+    public $sender;
+    public $message;
+    public $url;
 
     public function __construct()
     {
-        $this->client = new Client(env('TWILIO_SID', ''), env('TWILIO_TOKEN'));
-    }
-
-    public function SMS($to, $body, $from = null)
-    {
-        if($from === null)
+        if(is_null(config('services.twilio')) || config('services.twilio') === "")
         {
-            $from = env('TWILIO_NUMBER');
+            throw new Exception('Twilio not found in services.php');
+        }
+        elseif(is_null(config('services.twilio.sid')) || config('services.twilio.sid') === "")
+        {
+            throw new Exception('Twilio SID is not provided.');
+        }
+        elseif(is_null(config('services.twilio.token')) || config('services.twilio.token') === "")
+        {
+            throw new Exception('Twilio token is not provided.');
         }
 
-        $to = str_replace('+', '', $to);
-
-        $sms = $this->client->messages->create(
-            $to,
-            array(
-                'from' => $from,
-                'body' => $body,
-            )
-        );
-
-        return $sms;
+        $this->client = new Client(config('services.twilio.sid'), config('services.twilio.token'));
     }
-
-    public function MMS($to, $body, $url, $from = null)
+    
+    private function issetMethod()
     {
-        if($from === null)
+        if(isset($this->method))
         {
-            $from = env('TWILIO_NUMBER');
+            throw new Exception('You cannot redefine the twilio method');
         }
 
-        $to = str_replace('+', '', $to);
-
-        $mms = $this->client->messages->create(
-            $to,
-            array(
-                'from' => $from,
-                'body' => $body,
-                'mediaUrl' => $url,
-            )
-        );
-
-        return $mms;
+        return true;
     }
 
-    public function Call($to, $url, $from = null)
+    private function checkNumber($number)
     {
-        if($from === null)
+        $number = str_replace('+' , '', $number);
+
+        if(!is_numeric($number))
         {
-            $from = env('TWILIO_NUMBER');
+            throw new Exception('One of the phone numbers you provided is not valid.');
         }
 
-        $to = str_replace('+', '', $to);
+        return $number;
+    }
 
-        $call = $this->client->calls->create(
-            $to,
-            $from,
-            array(
-                'url' => $url
-            )
-        );
+    public function SMS()
+    {
+        Twilio::issetMethod();
 
-        return $call;
+        $this->method = "SMS";
+
+        return $this;
+    }
+
+    public function MMS()
+    {
+        Twilio::issetMethod();
+
+        if(isset($this->method))
+        {
+            throw new Exception('You cannot redefine the twilio method');
+        }
+
+        $this->method = "MMS";
+
+        return $this;
+    }
+
+    public function CALL()
+    {
+        Twilio::issetMethod();
+
+        $this->method = "CALL";
+
+        return $this;
+    }
+
+    public function from($number)
+    {
+        $this->sender = Twilio::checkNumber($number);
+
+        return $this;
+    }
+
+    public function to($number)
+    {
+        $this->recipient = Twilio::checkNumber($number);
+
+        return $this;
+    }
+
+    public function message($message)
+    {
+        $this->message = $message;
+        return $this;
+    }
+
+    public function url($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function start()
+    {
+        if($this->method === "SMS" && !isset($this->message))
+        {
+            throw new Exception('No message is provided for SMS.');
+        }
+        elseif($this->method === "CALL" && !isset($this->url))
+        {
+            throw new Exception('No TwiML URL is provided for call.');
+        }
+        elseif($this->method === "MMS")
+        {
+            if(!isset($this->url))
+            {
+                throw new Exception('No image URL is provided for MMS.');
+            }
+            elseif(!isset($this->message))
+            {
+                throw new Exception('No message is provided for MMS.');
+            }
+        }
+
+        if(!isset($this->sender))
+        {
+            if(is_null(config('services.twilio.number')) || config('services.twilio.number') === "")
+            {
+                throw new Exception('No sender phone number is provided.');
+            }
+
+            $this->sender = config('services.twilio.number');
+        }
+
+        if($this->method === "SMS" || $this->method === "MMS")
+        {
+            $data = [
+                'from' => $this->sender,
+                'body' => $this->message,
+            ];
+
+            if($this->method === "MMS")
+            {
+                $data['mediaUrl'] = $this->url;
+            }
+
+            $response = $this->client->messages->create($this->recipient, (array) $data);
+        }
+        elseif($this->method === "CALL")
+        {
+            $response = $this->client->calls->create($this->recipient, $this->sender, array('url' => $this->url));
+        }
+
+        return $response;
     }
 }
--- src/Facades/Twilio.php
+++ src/Facades/Twilio.php
@@ -2,7 +2,6 @@
 
 namespace Sigfriedseldeslachts\twiliosms\Facades;
 
-use Twilio\Rest\Client;
 use Illuminate\Support\Facades\Facade;
 
 class Twilio extends Facade
